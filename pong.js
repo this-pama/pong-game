3a1f9509cfedc101b9492b587f245f7367d24d36
adedapopaul <adedapopaul@yahoo.com>
2016-10-26 12:57:52 +0100

    Defining the logic of the pong page

---
+++ pong.js
@@ -0,0 +1,182 @@
+var canvas;
+var canvasContext;
+var ballX = 30;
+var ballY = 30;
+var ballSpeedX = 5;
+var ballSpeedY = 1;
+
+var player1Score = 0;
+var player2Score = 0;
+const WINNING_SCORE = 11;
+
+var showWinScreen = false;
+
+var paddle1Y = 250;
+var paddle2Y = 250;
+const PADDLE_THICKNESS = 10;
+const PADDLE_HEIGHT = 100;
+var collision = null;
+var finish = null;
+
+//starts game only after all other assests have been loaded
+window.onload = function() {
+    canvas = document.getElementById('gameCanvas');
+	canvasContext = canvas.getContext('2d');
+	collision = document.getElementById("collide");
+	finish = document.getElementById("finish");
+
+	var framesPerSecond = 60;
+
+  	setInterval(function() {
+			moveBallAndPaddle();
+			drawEverything();	
+		}, 1000/framesPerSecond);
+
+	canvas.addEventListener('mousedown', handleMouseClick);
+
+	canvas.addEventListener('mousemove',
+		function(evt) {
+			var mousePos = calculateMousePos(evt);
+			paddle1Y = mousePos.y - (PADDLE_HEIGHT/2);
+		});
+}
+
+//resets the ball to the center
+function ballReset() {
+	if(player1Score >= WINNING_SCORE ||
+		player2Score >= WINNING_SCORE) {
+
+		finish.play();
+		showWinScreen = true;
+
+	}
+
+	ballSpeedX = -ballSpeedX;
+	ballX = canvas.width/2;
+	ballY = canvas.height/2;
+}
+
+
+
+//move the ball and paddles
+function moveBallAndPaddle() {
+	if(showWinScreen) {
+		return;
+	}
+
+	computerMovement();
+
+	ballX = ballX + ballSpeedX;
+	ballY = ballY + ballSpeedY;
+	
+	if(ballX < 15) {
+		if(ballY > paddle1Y && ballY < paddle1Y+PADDLE_HEIGHT) {
+			
+
+			ballSpeedX = -ballSpeedX;
+			collision.play();
+			var deltaY = ballY-(paddle1Y+PADDLE_HEIGHT/2);
+			ballSpeedY = deltaY * 0.10;
+
+		} else {
+			player2Score++; 
+			ballReset();
+		}
+	}
+
+	if(ballX > (canvas.width - 15) ) {
+		if(ballY > paddle2Y && ballY < paddle2Y+PADDLE_HEIGHT) {
+			
+			ballSpeedX = -ballSpeedX;
+			collision.play();
+			var deltaY = ballY-(paddle2Y+PADDLE_HEIGHT/2);
+			ballSpeedY = deltaY * 0.10;
+		} else {
+			player1Score++; 
+			ballReset();	
+		}
+	}
+
+	if(ballY < 0 || ballY > canvas.height) {
+		ballSpeedY = -ballSpeedY;
+	}
+}
+
+//draws the net 
+function drawNet() {
+	for(var i=0;i<canvas.height;i+=40) {
+		colorRect(canvas.width/2-1,i,2,20,'white');
+	}
+}
+
+//draws the ball and paddles
+function drawEverything() {
+	// next line blanks out the screen with black
+	colorRect(0,0,canvas.width,canvas.height, "#0a181c");
+
+	if(showWinScreen) {
+		canvasContext.fillStyle = 'white';
+
+		if(player1Score >= WINNING_SCORE) {
+			;
+			canvasContext.font = "50px serif"
+			canvasContext.fillText("You won!", 300, 200);
+		} else if(player2Score >= WINNING_SCORE) {
+			canvasContext.font = "50px serif"
+			canvasContext.fillText("Computer won!", 250, 200);
+		}
+		canvasContext.font = "25px serif"
+		canvasContext.fillText("Click anywhere to restart", 300, 500);
+		return;
+	}
+
+  //ensures net is drawn before  paddles and ball
+	drawNet();
+
+	// this is left player paddle
+	colorRect(0,paddle1Y,PADDLE_THICKNESS,PADDLE_HEIGHT,'brown');
+
+	// this is right computer paddle
+	colorRect(canvas.width-PADDLE_THICKNESS,paddle2Y,PADDLE_THICKNESS,PADDLE_HEIGHT,'brown');
+
+	// next line draws the ball
+	colorCircle(ballX, ballY, 10, 'white');
+	canvasContext.font = "35px serif"
+	canvasContext.fillText(player1Score, 100, 100);
+	canvasContext.fillText(player2Score, canvas.width-100, 100);
+}
+
+//draws a circle
+function colorCircle(centerX, centerY, radius, drawColor, fontType) {
+	canvasContext.fillStyle = drawColor;
+	canvasContext.beginPath();
+	canvasContext.arc(centerX, centerY, radius, 0,Math.PI*2,true);
+	canvasContext.fill();
+}
+
+//draws a rectangle
+function colorRect(leftX,topY, width,height, drawColor) {
+	canvasContext.fillStyle = drawColor;
+	canvasContext.fillRect(leftX,topY, width,height);
+}
+
+//calculates mouse position
+function calculateMousePos(evt) {
+	var rect = canvas.getBoundingClientRect();
+	var root = document.documentElement;
+	var mouseX = evt.clientX - rect.left - root.scrollLeft;
+	var mouseY = evt.clientY - rect.top - root.scrollTop;
+	return {
+		x:mouseX,
+		y:mouseY
+	};
+}
+
+function handleMouseClick(evt) {
+	if(showWinScreen) {
+		player1Score = 0;
+		player2Score = 0;
+		showWinScreen = false;
+	}
+}
+
